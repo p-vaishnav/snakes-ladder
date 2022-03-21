97f7375dfdf7ad8e66552167826c7a6f55dc167f
SamyakJain2020 <samyakjainking@gmail.com>
2022-03-21 17:57:54 +0530

    cleaning code  + formatting

--- hack-snakes-and-ladders/src/App.js
+++ hack-snakes-and-ladders/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Board from './components/board/Board';
+import { useState } from "react";
+import "./App.css";
+import Board from "./components/board/Board";
 
 function App() {
+  const [numPlayers, setNumPlayers] = useState(null);
+
   return (
-    <Board rows={10} cols={10} boardSize={50} maxPlayers={2}/>    
+    <div className="App">
+    <header>
+        <h1>Welcome to Snake and Ladders</h1>
+    </header>
+    {
+      !numPlayers && (
+        <div>
+        <p>How many players?</p>
+        <button onClick={() => setNumPlayers(2)}>2</button>
+        <button onClick={() => setNumPlayers(3)}>3</button>
+        <button onClick={() => setNumPlayers(4)}>4</button>
+        </div>
+      
+      )
+
+    }
+      {numPlayers && <Board rows={10} cols={10} boardSize={50} maxPlayers={numPlayers}/>}
+    </div>
   );
 }
+
+
 
 export default App;
